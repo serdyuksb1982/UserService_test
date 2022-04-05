8cb177ee02494d977950af134b691c6739698f19
Sergey <Serduyksb@gimai.com>
2022-04-05 15:18:16 +0300

    Create User roles ADMIN

--- src/main/java/ru/serdeveloper/skllsApi/controller/UserController.java
+++ src/main/java/ru/serdeveloper/skllsApi/controller/UserController.java
@@ -2,6 +2,7 @@ package ru.serdeveloper.skllsApi.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import ru.serdeveloper.skllsApi.repository.UserRepo;
@@ -17,7 +18,9 @@ public class UserController {
     private UserRepo userRepo;
 
     @GetMapping
-    private String userList() {
+    private String userList(Model model) {
+        model.addAttribute("users", userRepo.findAll());
+
         return "userList";
     }
 }
