e68fe687a94a8068c8613561eade78209edaa012
Sergey <Serduyksb@gimai.com>
2022-04-15 11:43:06 +0300

    Validation: messageType in success or danger email validation

--- src/main/java/ru/serdeveloper/skllsApi/controller/RegistrationController.java
+++ src/main/java/ru/serdeveloper/skllsApi/controller/RegistrationController.java
@@ -3,10 +3,12 @@ package ru.serdeveloper.skllsApi.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import ru.serdeveloper.skllsApi.domian.User;
 import ru.serdeveloper.skllsApi.service.UserService;
 
@@ -27,12 +29,23 @@ public class RegistrationController {
     }
 
     @PostMapping("/registration")
-    public String addUser(@Valid User user, BindingResult bindingResult, Model model) {
-        if (user.getPassword() != null && !user.getPassword().equals(user.getPassword2())) {
+    public String addUser(
+                          @RequestParam("password2") String passwordConfirm,
+                          @Valid User user,
+                          BindingResult bindingResult,
+                          Model model)
+    {
+        boolean isConfirmEmpty = StringUtils.isEmpty(passwordConfirm);
+
+        if(isConfirmEmpty) {
+            model.addAttribute("password2Error", "Password confirmation cannot be empty");
+        }
+
+        if (user.getPassword() != null && !user.getPassword().equals(passwordConfirm)) {
             model.addAttribute("passwordError", "Passwords are different!");
         }
 
-        if (bindingResult.hasErrors()) {
+        if (isConfirmEmpty || bindingResult.hasErrors()) {
             Map<String, String> errors = ControllerUtils.getErrors(bindingResult);
 
             model.mergeAttributes(errors);
@@ -53,11 +66,12 @@ public class RegistrationController {
         boolean isActivated = userService.activateUser(code);
 
         if (isActivated) {
+            model.addAttribute("messageType", "success");
             model.addAttribute("message", "User successfully activated");
         } else {
+            model.addAttribute("messageType", "danger");
             model.addAttribute("message", "Activation code is not found!");
         }
-
         return "login";
     }
 }
--- src/main/java/ru/serdeveloper/skllsApi/domian/User.java
+++ src/main/java/ru/serdeveloper/skllsApi/domian/User.java
@@ -27,9 +27,7 @@ public class User implements UserDetails {
     private String username;
     @NotBlank(message = "Password cannot be empty")
     private String password;
-    @Transient
-    @NotBlank(message = "Password confirmation cannot be empty")
-    private String password2;
+    //deleted password2 in User entity
 
     private boolean active;
     @Email(message = "Email is not correct!")
